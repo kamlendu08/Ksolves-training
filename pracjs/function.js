b9c0247fb37115fad73be0cfcaf863128c1f65f2
kamlendu08 <kamlendubala@gmail.com>
2025-01-23 15:19:55 +0530

    function

---
+++ pracjs/function.js
@@ -0,0 +1,16 @@
+function add(a, b) {
+  return a + b;
+}
+
+console.log(add(3, 4));
+
+let x = 3
+if (x > 10) {
+  console.log('x is greater than 10');
+} else {
+  console.log('x is less than or equal to 10');
+}
+
+for (let i = 0; i < 5; i++) {
+  console.log(i);  // Output: 0, 1, 2, 3, 4
+}
