4a58d6c2d50bd1b3a73f3a0745fee05abb4f55b6
kamlendu08 <kamlendubala@gmail.com>
2025-02-12 16:03:04 +0530

    commit

--- dbconnect/index.js
+++
@@ -1,8 +0,0 @@
-const dbc = require("./db");
-const express = require("express");
-const app = express();
-app.use(express.json());
-
-
-
-app.listen(3000, () => { console.log("server connected") });
